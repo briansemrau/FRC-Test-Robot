f80b1e314f4daa7ee7b1000245dabd4820ec6071
Brian Semrau <brian.semrau@gmail.com>
2016-11-11 07:54:52 -0500

    Base Iterative Robot Project

---
+++ src/org/usfirst/frc/team4737/robot/Robot.java
@@ -0,0 +1,47 @@
+
+package org.usfirst.frc.team4737.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to each mode, as
+ * described in the IterativeRobot documentation. If you change the name of this class or the package after creating
+ * this project, you must also update the manifest file in the resource directory.
+ */
+public class Robot extends IterativeRobot {
+
+    /**
+     * This function is run when the robot is first started up and should be used for any initialization code.
+     */
+    public void robotInit() {
+        System.out.println("hello world, i'm a robot");
+    }
+
+    public void autonomousInit() {
+
+    }
+
+    public void autonomousPeriodic() {
+
+    }
+
+    public void teleopInit() {
+
+    }
+
+    public void teleopPeriodic() {
+
+    }
+
+    public void testInit() {
+
+    }
+
+    public void testPeriodic() {
+
+    }
+
+}
